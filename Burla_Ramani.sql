38025b9725b72da181ecf0303aa0c5fc9d9088ed
Burla-Ramani <boorlaramani@gmail.com>
2023-07-28 14:45:52 +0000

    final codes

--- Burla_Ramani.sql
+++ Burla_Ramani.sql
@@ -1,5 +1,5 @@
 set timing on;
--- ---(1.How many products were sold in february 2019)
+-1.
 
 select  SUM(Quantity) as soldproducts  
 from ecommerce where "PURCHASE_DATE" >='2019-02-01' 
@@ -10,7 +10,7 @@ AND "PURCHASE_DATE" <'2019-03-01';
 -- --------------------------------------------------------
 
 
--- (2.Sql query to total sale amount in each year)
+--2
 
 select EXTRACT(year from To_Date("PURCHASE_DATE",'YYYY-MM-DD'))
 as Sale_Year,sum(Quantity*Price) as Total_saleamount
@@ -19,8 +19,7 @@ from ECOMMERCE group by EXTRACT(year from To_Date("PURCHASE_DATE",'YYYY-MM-DD'))
 
 -- --------------------------------------------------------------------------
 
--- (3.sql query to what was the total sales amount of each product 
--- on a month-wise basis in the year 2019)
+-- 3
 
 select EXTRACT(month from To_Date("PURCHASE_DATE",'YYYY-MM-DD'))as month_sales,
 sum(Quantity*price)as total_sales,productno  from Ecommerce where 
@@ -52,14 +51,14 @@ GROUP BY
 
 -- -------------------------------------------------------------------------------
 
--- (4.Sql query to count the customers from each country)
+-- 4
 select count(DISTINCT CUSTOMERNO) as customer_count,country
  from ecommerce group by country;
 
 
 -- -------------------------------------------------------------------------------
 
--- (5.SQL query to list all the unique product names sold from each year)
+--5
 
 select distinct(PRODUCTNAME),EXTRACT(year from To_Date
 ("PURCHASE_DATE",'YYYY-MM-DD'))
@@ -71,7 +70,7 @@ as Sale_Year from ECOMMERCE;
 -- ----------------------------------------------------------------------
 -- OPTIMIZED CODES
 
---1).How many products were sold in february 2019)
+--1)
 select sum(quantity) as products_sold from ecommerce where (extract (year from to_date(Purchase_Date,'YYYY-MM-DD')))=2019 
 and (extract (month from to_date(Purchase_Date,'YYYY-MM-DD')))=2 ;
 
@@ -79,15 +78,14 @@ and (extract (month from to_date(Purchase_Date,'YYYY-MM-DD')))=2 ;
 
 
 
--- 2)Sql query to total sale amount in each year
+-- 2)
 select SUBSTR(Purchase_Date, 1, 4) as "Year",sum(PRICE*QUANTITY) 
 as total_sales_amount from ecommerce group by SUBSTR(Purchase_Date, 1, 4); 
 
 
 
 
--- 3)sql query to what was the total sales amount of each product 
--- on a month-wise basis in the year 2019
+-- 3)
 
 select SUBSTR(Purchase_Date, 6, 2) as "Month",PRODUCTNO,sum(PRICE*QUANTITY) as
  total_sales_amount from ECOMMERCE where SUBSTR(Purchase_Date, 1, 4)='2019' group by SUBSTR(Purchase_Date, 6, 2),PRODUCTNO;
@@ -95,13 +93,13 @@ select SUBSTR(Purchase_Date, 6, 2) as "Month",PRODUCTNO,sum(PRICE*QUANTITY) as
 
 
 
---  4)Sql query to count the customers from each country
+--  4)
 select COUNTRY,count( DISTINCT CUSTOMERNO) as count from ECOMMERCE group by COUNTRY; 
 
 
 
 
--- 5)SQL query to list all the unique product names sold from each year
+-- 5)
 SELECT DISTINCT PRODUCTNAME AS UNIQUE_PRODUCTS,EXTRACT(year FROM TO_DATE(Purchase_Date,'YYYY-MM-DD')) as YEAR_FIELD 
 FROM ecommerce; 
 
@@ -112,7 +110,7 @@ FROM ecommerce;
 -- WITH EXPLAIN COMMAND
 
 
---1).How many products were sold in february 2019)
+--1)
 
 explain plan set  statement_id='5'for select sum(quantity) as products_sold from ecommerce where (extract (year from to_date("PURCHASE_DATE",'YYYY-MM-DD')))=2019 
 and (extract (month from to_date("PURCHASE_DATE",'YYYY-MM-DD')))=2 ;
@@ -120,7 +118,7 @@ select * from table(DBMS_XPLAN.DISPLAY(null,'5','BASIC'));
 
 
 
--- 2)Sql query to total sale amount in each year
+-- 2)
 
 explain plan set  statement_id='6'for select SUBSTR("PURCHASE_DATE", 1, 4) as "Year",sum(PRICE*QUANTITY) 
 as total_sales_amount from ecommerce group by SUBSTR("PURCHASE_DATE", 1, 4); 
@@ -129,20 +127,19 @@ select * from table(DBMS_XPLAN.DISPLAY(null,'6','BASIC'));
 
 
 
--- 3)sql query to what was the total sales amount of each product 
--- on a month-wise basis in the year 2019
+-- 3)
 
 explain plan set  statement_id='7'for select SUBSTR("PURCHASE_DATE", 6, 2) as "Month",PRODUCTNO,sum(PRICE*QUANTITY) as
  total_sales_amount from ECOMMERCE where SUBSTR("PURCHASE_DATE", 1, 4)='2019' group by SUBSTR("PURCHASE_DATE", 6, 2),PRODUCTNO;
  select * from table(DBMS_XPLAN.DISPLAY(null,'7','BASIC'));
 
 
---  4)Sql query to count the customers from each country
+--  4)
 explain plan set  statement_id='8'for select COUNTRY,count( DISTINCT CUSTOMERNO) as count from ECOMMERCE group by COUNTRY;
 select * from table(DBMS_XPLAN.DISPLAY(null,'8','BASIC'));
 
 
--- 5)SQL query to list all the unique product names sold from each year
+-- 5)
 explain plan set  statement_id='9'for SELECT DISTINCT PRODUCTNAME AS UNIQUE_PRODUCTS,EXTRACT(year FROM TO_DATE("PURCHASE_DATE",'YYYY-MM-DD')) as YEAR_FIELD 
 FROM ecommerce; 
 select * from table(DBMS_XPLAN.DISPLAY(null,'9','BASIC'));
