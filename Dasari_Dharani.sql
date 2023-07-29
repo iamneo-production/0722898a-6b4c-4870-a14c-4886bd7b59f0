fb83a65bf93507a62dca0462f44b4f128e2d2da3
DharaniDasari <dasaridharani2@gmail.com>
2023-07-29 12:35:17 +0000

    Final Optimized Queries

--- Dasari_Dharani.sql
+++ Dasari_Dharani.sql
@@ -2,51 +2,20 @@ select * from ecommerce;
 -- desc ecommerce;
 set timing on;
 
---Question-1
-
-select sum(QUANTITY) as quantity FROM ECOMMERCE where extract(year from TO_DATE(PURCHASE_DATE,'YYYY-MM-DD'))=2019 
-and extract(month from TO_DATE(PURCHASE_DATE,'YYYY-MM-DD'))=02; 
-
---Question-2
-
-select sum(PRICE * QUANTITY) as total_sale_amount,
-extract(year from TO_DATE(PURCHASE_DATE,'YYYY-MM-DD')) AS "Year_Field"
-from ECOMMERCE group by extract(year from TO_DATE(PURCHASE_DATE,'YYYY-MM-DD'));
-
---Question-3
-
-select PRODUCTNO,extract(month from TO_DATE(PURCHASE_DATE,'YYYY-MM-DD')) as "Month_Field",sum(PRICE * QUANTITY) 
-as sale_amount from ecommerce 
-where extract(year from TO_DATE(PURCHASE_DATE,'YYYY-MM-DD'))='2019' group by productno, extract(month from TO_DATE(PURCHASE_DATE,'YYYY-MM-DD'));
-
---Question-4
-
-select COUNTRY,count( DISTINCT CUSTOMERNO) as count from ECOMMERCE group by COUNTRY; 
-
---Question-5
-
-select  Distinct PRODUCTNAME, extract(year from TO_DATE(Purchase_Date,'YYYY-MM-DD')) as year from ECOMMERCE ;
 
 --Final Optimized Code--
 
 -- Question 1
 
-SELECT SUM(quantity) AS TotalProducts_Sold
-FROM ecommerce
-WHERE EXTRACT(YEAR FROM TO_DATE("PURCHASE_DATE",'YYYY-MM-DD'))=2019 AND 
-EXTRACT(MONTH FROM TO_DATE("PURCHASE_DATE",'YYYY-MM-DD'))=02;
-
---or--
-
-SELECT SUM(quantity) AS TotalProducts_Sold
+SELECT SUM(quantity) AS Total_Products_Sold
 FROM ecommerce
 WHERE "PURCHASE_DATE" LIKE '2019-02%';
---second time it takes only 03 ms.
+
 
 -- Question 2
 
 SELECT SUBSTR("PURCHASE_DATE",1,4) AS "Year", 
-SUM(price * Quantity) AS TotalSalesAmount
+SUM(price * Quantity) AS Total_Sales_Amount
 FROM ECOMMERCE
 GROUP BY SUBSTR("PURCHASE_DATE",1,4);
 
@@ -55,7 +24,7 @@ GROUP BY SUBSTR("PURCHASE_DATE",1,4);
 SELECT 
 SUBSTR("PURCHASE_DATE",6,2) AS "Month",
 productno,
-SUM(price * Quantity) AS TotalSalesAmount
+SUM(price * Quantity) AS Total_Sales_Amount
 FROM ECOMMERCE
 WHERE "PURCHASE_DATE" LIKE '2019%'
 GROUP BY productno, 
@@ -66,7 +35,7 @@ SUBSTR("PURCHASE_DATE",6,2);
 SELECT 
 SUBSTR("PURCHASE_DATE",6,2) AS "Month",
 productno,
-SUM(price * Quantity) AS TotalSalesAmount
+SUM(price * Quantity) AS Total_Sales_Amount
 FROM ECOMMERCE
 WHERE SUBSTR("PURCHASE_DATE",1,4)='2019'
 GROUP BY productno, 
@@ -81,7 +50,7 @@ GROUP BY COUNTRY;
 -- Question 5
 
 SELECT DISTINCT productname AS UNIQUE_PRODUCTS, 
-EXTRACT(YEAR FROM TO_DATE("PURCHASE_DATE",'YYYY-MM-DD')) AS Year
+SUBSTR("PURCHASE_DATE",1,4) AS Year
 FROM ecommerce;
 
 -- Creating Index
